c7f0177d23a094b90c46abf8837d3be52b4c89e5
gfile <gfile@fun.gfile.com>
2017-09-04 18:55:57 +0900

    fix comment

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -8,51 +8,106 @@ use Request;
 class Products extends Controller
 {
 
-  //get /products or /
+  /**
+	get /products or /
+	
+	@return \Illuminate\Http\Response
+	
+	*/
   public function index()
   {
 		$products = Product::all();
     return view('products.index')->with('products',$products);
   }
 
-  //get /products/new
-  public function new()
+  /*
+	get /products/new
+  
+	@return \Illuminate\Http\Response
+	
+	*/
+	public function new()
   {
     return view('products.new');
   }
 
-	//post products/new
+	/**
+	
+	post products/new
+	
+	@param \Illuminate\Http\Request	$request
+
+	@return \Illuminate\Http\Response
+	
+	*/
+
+
 	public function create(Request $request)
 	{
 		Product::create(Request::all());
 		return redirect('/');
 	}
 
-	//get products/1
+	/**
+
+	get products/1
+	
+	@param int $id
+
+	@return \Illuminate\Http\Response
+		
+	*/
+
 	public function show($id)
 	{
 		$product = Product::find($id);
 		return view('products.show', compact('product'));	
 	}
-	//get products/1/edit
+	/**
+
+	get products/1/edit
+	
+	@param int $id
+	
+	@return \Illuminate\Http\Response
+
+	*/
+	
 	public function edit($id)
 	{
 		$product = Product::find($id);
 		return view('products.edit', compact('product'));
 	}
 
-	//post products/1/edit
+	/**
+	
+	post products/1/edit
+	
+	@param int $id
+	
+	@param \Illuminate\Http\Request $request
+	
+	@return \Illuminate\Http\Response
+
+	*/
 	public function update($id,Request $request)
 	{
 		Product::find($id)->update(Request::all());
 		return redirect('/');
 	}
 
-	//delete products/1	
+	/**
+
+	delete products/1	
+	
+	@param int $id
+	
+	@return \Illuminate\Http\Response
+
+	*/
 	public function destroy($id)
 	{
 		Product::find($id)->delete();	
     return redirect ('/');
 	}
-
 }
