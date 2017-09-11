ed1743835df66b6f091c7a7a8bcc71e968448614
gfile <gfile@fun.gfile.com>
2017-09-11 17:53:00 +0900

    fix index farewell bootstrap

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,35 +7,14 @@
 </div>
 <hr/>
 {!! $products->render() !!}
-<div class="table-responsive">
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>image</th>
-                <th>title and description</th>
-                <th>rating</th>
-                <th colspan="3"></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($products as $product)
-            <tr>
-                <td><img src="{{ asset('storage/productimage/' .$product->image_filename) }}" alt="image"/></td>
-                <td>
-                    <h2>{!!$product->title!!}</h2>
-                    <h4>{!!$product->description!!}</h4>
-                </td> 
-                <td>
-                    @if ($product->rating>50)
-                        <div class="animerating-good">
-                    @else
-                        <div class="animerating-bad">
-                    @endif
-                    <h3>{!!$product->rating!!}</h3></div></td> 
-                <td>{{ link_to_action('Products@show', 'show this anime', [$product->id], ['class' => 'btn btn-default']) }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    @foreach($products as $product)
+        <div class="index-box">
+            {{ link_to_action('Products@show', 'show this anime', [$product->id]) }}
+            <img src="{{ asset('storage/productimage/' .$product->image_filename) }}" alt="image"/>
+            <h2>{!!$product->title!!}</h2>
+            <h4>{!!$product->description!!}</h4>
+            <br> 
+        </div>
+        <hr>
+    @endforeach    
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,15 +1,22 @@
 @extends('../shared/application')
 @section('title', "show")
 @section('content')
-{!!'title: '.$product->title!!}
-<br>
-{!!'description: '.$product->description!!}
-<br>
-@if (!!$product->image_filename && $product->image_filename != "noimage")
+@if (!!$product->image_filename && $product->image_filename != "noimage.png")
     <img src="{{ asset('storage/productimage/' .$product->image_filename) }}" alt="image" />
     <br>
 @endif
-
+<h2>{!!$product->title!!}</h2>
+<br>
+</h4>{!!$product->description!!}</h4>
+<br>
+@if ($product->rating>50)
+    <div class="animerating-good">
+@else
+    <div class="animerating-bad">
+@endif
+        <h4>{!!$product->rating!!}</h4>
+    </div> 
+<br>
 {{ link_to_action('Products@index', 'back', [], ['class' => 'btn btn-default']) }}
 <br>
 <div class="text-right">
